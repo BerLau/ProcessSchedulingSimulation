44ee0bdfd45bfa064af6604e3ceec06b4e54c0b5
BerLau <bernard.lau999@gmail.com>
2017-05-26 11:20:02 +0800

    interruptRegister

--- ProcessSchedulingSimulation/InterruptRegister.cpp
+++ ProcessSchedulingSimulation/InterruptRegister.cpp
@@ -4,6 +4,60 @@
 
 InterruptRegister::InterruptRegister()
 {
+	recorder = 0;
+}
+
+bool InterruptRegister::hasRecord()
+{
+	return recorder;
+}
+
+void InterruptRegister::setRecord(ItrpType type)
+{
+	switch (type)
+	{
+	case TIMEOUT:recorder |= 1;
+		break;
+	case CREATED:recorder |= 2;
+		break;
+	case BLOCKED:recorder |= 4;
+		break;
+	case FINISHED:recorder |= 8;
+		break;
+	case REVIVED:recorder |= 16;
+		break;
+	default:
+		break;
+	}
+}
+
+void InterruptRegister::reset()
+{
+	recorder = 0;
+}
+
+void InterruptRegister::reset(ItrpType type)
+{
+	switch (type)
+	{
+	case TIMEOUT:recorder ^= 1;
+		break;
+	case CREATED:recorder ^= 2;
+		break;
+	case BLOCKED:recorder ^= 4;
+		break;
+	case FINISHED:recorder ^= 8;
+		break;
+	case REVIVED:recorder ^= 16;
+		break;
+	default:
+		break;
+	}
+}
+
+ItrpType InterruptRegister::getItrpType()
+{
+	int type=recorder
 }
 
 
--- ProcessSchedulingSimulation/entrace.cpp
+++ ProcessSchedulingSimulation/entrace.cpp
@@ -3,10 +3,13 @@
 using namespace std;
 int main() {
 	ofstream fout;
+	int a = 13;
+	int b = 4;
+	int c = a^b;
 	fout.open("D:\\debugLogs\\pss.log", ios::app);
 	if (fout.is_open())
 	{
-		fout << "hello world";
+		fout <<c<<endl;
 		fout.close();
 	}
 	cin.get();
